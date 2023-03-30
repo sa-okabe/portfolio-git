8ac9443e432bc098a33f728ce7a104e4d37d3c14
shigeru.hyodo <41170561gel1123@users.noreply.github.com>
2023-03-30 14:59:24 +0900

    fix: アニメーションのスタイル定義がなかったので追加＆軽微な修正（これでアニメーションのCSSが付与されるようになったが、おそらくまだアニメーションとして見た目の変化がないはず。もう一息！）

--- myportfolio/pages/about/about.vue
+++ myportfolio/pages/about/about.vue
@@ -3,14 +3,14 @@
     <div
         class="relative h-96 w-full mt-10 bg-no-repeat bg-cover bg-center drop-shadow-2xl bg-[url('~/assets/birmingham-museums-trust-p00MgXaa6as-unsplash.jpg')] animate-scale-in-center">
         <h1 class="font-serif">ab0ut</h1>
-        <diV class="font-serif font-semibold text-lg mt-6 ml-4 text-white">
+        <div class="font-serif font-semibold text-lg mt-6 ml-4 text-white">
             <p>イギリスの大学でビジネスコミュニケーション学、マーケティングなどを学ぶ。</p>
             <p>就職活動時の自己分析をした上で気づいた、『物事の仕組みそのもの』が好きなこと、『自分の手で何かを生み出していきたい』を活かすため、</p>
             <p>エンジニアとして前会社に入社。</p>
             <p>新しい技術を取り入れた開発業務に携わりたく、会社に中途入社。</p>
             <p>より簡単で、新しいサービス開発に向けて、それを実現しうる技術の習得のため、日々奮闘中。</p>
             <p class="mt-10">ミステリ小説とファッションとゾンビ映画がすき。</p>
-        </diV>
+        </div>
     </div>
 
     <div class="object-cover h-28 w-10 flex">
